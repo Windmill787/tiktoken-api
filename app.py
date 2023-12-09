a4e8174345964c97d4f7b8b9fec4e1fb171179ea
Stolyarov Max <Windmill78max@gmail.com>
2023-12-09 21:55:56 +0100

    Code refactor

--- app.py
+++ app.py
@@ -15,22 +15,15 @@ load_dotenv(override=True)
 
 app = Flask(__name__)
 
+encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
+
 
 @app.route('/', methods=['POST'])
 def index():
-    # get text from form
-    form = request.get_json()
-
-    if "text" not in form:
+    if "text" not in request.get_json():
         return jsonify({'message': 'Text is empty'})
 
-    text = form["text"]
-
-    encoding = tiktoken.encoding_for_model('gpt-3.5-turbo')
-
-    num_tokens = len(encoding.encode(text))
-
-    return jsonify({'count': num_tokens})
+    return jsonify({'count': len(encoding.encode(request.get_json()["text"]))})
 
 if __name__ == '__main__':
     app.run(debug=(os.getenv('FLASK_DEBUG') == 'True'), port=os.getenv('FLASK_PORT'), host="0.0.0.0")
